8504c4cbd82772dfc7dcbc01eb8b1a9d6cd9c345
gongbiao <developbiao@gmail.com>
2018-10-17 22:05:07 +0800

    Close session by with.

--- 1016/constant.py
+++ 1016/constant.py
@@ -17,3 +17,14 @@ result = sess.run(multiple)
 
 # 用print打印矩阵乘法的结果
 print(result)
+
+if const1.graph is tf.get_default_graph():
+	print("const1所在的图 (Graph)是当前default graph")
+
+# close session
+sess.close()
+
+# 第二第方法来创建和关闭Session
+with tf.Session() as sess:
+	result2 = sess.run(multiple)
+	print("Multiple的结果是:%s " % result2)
